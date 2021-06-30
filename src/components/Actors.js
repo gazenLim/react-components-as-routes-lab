5ed7489c3d2e15f3ec90af3e38e772592bd4f169
gajen <meetgajen@yahoo.com>
2021-06-30 15:18:22 -0400

    commit tree 879bdb7a3a3cc8c88ac55c719c56da30cb2cf226

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  console.log(actors)
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return <div key={actor.name}>
+          <h3>{actor.name}</h3>
+          <ul><strong>Movies</strong></ul>
+          {actor.movies.map(movie =>{
+            return <li>{movie}</li>
+          })}
+          <hr/>
+        </div>
+        
+      })}
+      
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  console.log(movies)
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+    {movies.map(movie =>{
+      return <div>
+        <h3>{movie.title}</h3>
+        <p>Length: {movie.time}</p>
+        <p>Genres:</p>
+        <ul>
+          {"" + movie.genres.join(', ')}
+        </ul>
+
+
+
+      </div>
+    })}
+        
+
+
+
     </div>
   );
 };
